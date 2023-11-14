47e6047bf1955a89bd6468b21065dd3d578d0d14
Gmprado78 <143112980+Gmprado78@users.noreply.github.com>
2023-11-14 10:38:25 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,3 +18,16 @@ function initmap() {
 	  zoom 8
   });
 }
+
+let map;
+
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+initMap();
