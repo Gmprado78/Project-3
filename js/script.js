0f0f4a357121b860dcbbcd480e45b8cc06309e11
Gmprado78 <Gmprado@cps.edu>
2023-11-14 21:21:28 -0600

    Success

--- js/script.js
+++ js/script.js
@@ -1,7 +1,39 @@
-var map;
-		function initmap() {
-		  map = new google.maps.Map(document.getElementById('map'), {
-		    center: {lat: -34.397, lng: 150.644},
-			zoom: 8
-		  });
-		 }
+function init() {
+	alert('it works');
+	
+}
+
+google.maps.event.addDomListener(window, 'load', init);
+
+
+function initMap() {
+  const myLatLng = { lat: 41.76999521182157, lng: -87.70415171237143};
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: myLatLng,
+  });
+
+  new google.maps.Marker({
+    position: myLatLng,
+    map,
+    title: "Hello World!",
+  });
+}
+
+window.initMap = initMap;
+
+let map;
+
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat:  41.76999521182157, lng:-87.70415171237143},
+    zoom: 8,
+  });
+}
+
+initMap();
+
+
+
