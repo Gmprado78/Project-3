64665992a8474e58f65c61320349a6c387d655cf
Gmprado78 <Gmprado@cps.edu>
2023-11-26 16:30:13 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,29 +11,13 @@ let labelIndex = 0;
 function initMap() {
   const myLatLng = { lat: 41.76999521182157, lng: -87.70415171237143};
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
     center: myLatLng,
+	zoom:15,
   });
 
   new google.maps.Marker({
     position: myLatLng,
     map,
-    title: "Hello World!",
+    title: "Marquette Park",
   });
 }
-
-window.initMap = initMap;
-
-let map;
-
-async function initMap() {
-  const { Map } = await google.maps.importLibrary("maps");
-
-  map = new Map(document.getElementById("map"), {
-    center: { lat:  41.76999521182157, lng:-87.70415171237143},
-    zoom: 15,
-  });
-}
-
-initMap();
-
